d8d6e33f1ca86eaf8fb30ca7422669dd9f19ec70
Arne <118668044+Roeli1996@users.noreply.github.com>
2026-02-16 08:29:34 +0100

    Update config_flow.py

--- custom_components/sleep_mqtt/config_flow.py
+++ custom_components/sleep_mqtt/config_flow.py
@@ -3,14 +3,10 @@ from homeassistant import config_entries
 from .const import DOMAIN
 
 class SleepAsAndroidConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Custom SleepAsAndroid MQTT Sensors."""
-    
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step when adding via UI."""
         if user_input is not None:
-            # Voorkom dubbele configuraties voor hetzelfde topic
             unique_id = f"{DOMAIN}_{user_input['topic']}"
             await self.async_set_unique_id(unique_id)
             self._abort_if_unique_id_configured()
@@ -20,11 +16,10 @@ class SleepAsAndroidConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 data=user_input
             )
 
-        # Het formulier met standaardwaarden voor Roeli1996
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("device_name", default="User-slaap"): str,
-                vol.Required("topic", default="SleepAsAndroid/User"): str,
+                vol.Required("device_name", default="Slaap Roeli"): str,
+                vol.Required("topic", default="SleepAsAndroid/Roeli"): str,
             })
         )
